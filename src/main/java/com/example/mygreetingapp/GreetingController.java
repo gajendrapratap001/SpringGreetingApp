8326da22007156fe1feedd8b24e3cc3d4be05e57
Gajendra_Pratap_Singh <gajendrapratapsingh0405@gmail.com>
2025-03-03 16:24:31 +0530

    [Gajendra] Added: Message with first and last name

--- src/main/java/com/example/mygreetingapp/GreetingController.java
+++ src/main/java/com/example/mygreetingapp/GreetingController.java
@@ -31,8 +31,9 @@ public class GreetingController {
     }
 
     @GetMapping
-    public String getGreeting() {
-        return greetingService.getGreeting();
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreeting(firstName, lastName);
     }
 
 }
