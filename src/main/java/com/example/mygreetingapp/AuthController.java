7ea4dc3485a06d31777ac56d68ead682b66b0dc0
Gajendra_Pratap_Singh <gajendrapratapsingh0405@gmail.com>
2025-03-05 11:16:29 +0530

    [Gajendra] Added: Added Swagger UI

--- src/main/java/com/example/mygreetingapp/AuthController.java
+++ src/main/java/com/example/mygreetingapp/AuthController.java
@@ -3,20 +3,27 @@ package com.example.mygreetingapp;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication API", description = "Endpoints for user authentication")
 public class AuthController {
 
     @Autowired
     private AuthenticationService authenticationService;
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user", description = "Registers a user and sends a welcome email")
+
     public String registerUser(@Valid @RequestBody AuthUserDTO authUserDTO) {
         return authenticationService.registerUser(authUserDTO);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login user", description = "Authenticates a user and returns a success message")
+
     public String loginUser(@Valid @RequestBody LoginDTO loginDTO) {
         return authenticationService.loginUser(loginDTO);
     }
